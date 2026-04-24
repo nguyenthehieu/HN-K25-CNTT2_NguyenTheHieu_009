bdd68a28c66114c3c81f63db4e8dc47e04309f00
Hiếu <thehieu090107@gmail.com>
2026-04-24 15:25:37 +0700

    15:25

--- hackathon.sql
+++ hackathon.sql
@@ -52,10 +52,10 @@ values
 insert into flights(flight_id, route_name, airline_id, ticket_price, available_seats)
 values 
 ('F01', 'HN-HCM', 'A01', '2500000', 50),
-('F02', 'HN-DN', 'A02', '1500000', 30),
-('F03', 'HCM-DN', 'A03', '1200000', 40),
-('F04', 'HN-PQ', 'A04', '3000000', 20),
-('F05', 'HCM-DL', 'A05', '1000000', 15);
+('F02', 'HN-DN', 'A01', '1500000', 30),
+('F03', 'HCM-DN', 'A02', '1200000', 40),
+('F04', 'HN-PQ', 'A03', '3000000', 20),
+('F05', 'HCM-DL', 'A04', '1000000', 15);
 
 insert into bookings(passenger_id, flight_id, booking_status, booking_date)
 values
@@ -67,9 +67,9 @@ values
 
 -- Chuyến bay 'HN-PQ' vừa được tăng cường tàu bay lớn hơn, hãy tăng available_seats thêm 10 ghế và tăng ticket_price lên 5%.
 update flights 
-set available_seats = available_seats + 10 and ticket_price = ticket_price * 1.05
-where route_name = 'HN-PQ';
-
+set available_seats = 30 and ticket_price = ticket_price * 1.05
+where route_name like 'HN-PQ%';
+select * from flights;
 -- Cập nhật số điện thoại của hành khách có passenger_id = 'P03' thành '0999999999'.
 update passengers 
 set phone = '0999999999'
@@ -97,8 +97,7 @@ order by booking_date desc;
 -- Lấy ra 3 chuyến bay có giá vé (ticket_price) đắt nhất trong hệ thống.
 select route_name, ticket_price 
 from flights
-where max(ticket_price)
-limit 3;
+where max(ticket_price);
 
 -- Hiển thị danh sách route_name, available_seats từ bảng Flights, bỏ qua 2 chuyến bay đầu tiên và lấy 2 chuyến bay tiếp theo (Phân trang).
 select route_name, available_seats
@@ -109,10 +108,10 @@ limit 2 offset 2;
 -- Chỉ lấy những vé đang có trạng thái 'Booked'.
 select booking_id, booking_date
 from bookings b
-inner join passengers p on b.passenger_id = p.full_name
-inner join flights f on b.flight_id = f.route_name
+join passengers p on b.passenger_id = p.full_name
+join flights f on b.flight_id = f.route_name
 where booking_status = 'Booked';
 
 -- Liệt kê tất cả các Hãng hàng không (Airlines) và tên chặng bay (route_name) thuộc hãng đó. 
 -- Hiển thị cả những hãng chưa có chuyến bay nào khai thác.
--- ????????????
+
